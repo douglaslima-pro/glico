1071369c67f26e0ade6eb4270163cc430c5450d0
douglaslima-pro <douglaslima-pro@outlook.com>
2023-09-30 22:15:37 -0300

    feat: fix recuperar-senha.js script

--- view/js/recuperar-senha.js
+++ view/js/recuperar-senha.js
@@ -5,7 +5,7 @@ function recuperarSenha(inputID,submitID){
     let emailusuario = document.getElementById(inputID).value;
     let submit = document.getElementById(submitID);
     //desabilita o submit até processar toda a solicitação
-    submit.disable = true;
+    submit.disabled = true;
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST","../../controller/recuperarSenha.php",true);
@@ -15,10 +15,10 @@ function recuperarSenha(inputID,submitID){
             console.log(xhr.responseText);
             if(xhr.responseText == "true"){
                 aviso(`sucesso:<b>Sucesso</b><br>Enviamos um link para alterar a senha no seu e-mail!`);
-                submit.disable = false;
+                submit.disabled = false;
             }else{
                 aviso(`erro:<b>Erro</b><br>Não encontramos uma conta ativa para <b>${emailusuario}</b>!`);
-                submit.disable = false;
+                submit.disabled = false;
             }
         }
     };
